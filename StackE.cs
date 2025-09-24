242fa4ae8cdfc2a63a82a2d7595c1c424b118931
kobehoste <kobe.hoste@campuskompas.be>
2025-09-24 15:55:42 +0200

    Add files via upload
    oefening voor bzw 24/09

---
+++ StackE.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace ThuisOefeningen;
+
+public class StackE
+
+{
+
+    static void Main(String[] args)
+    {
+        Stack myStack = new Stack();
+        myStack.Push("Fiona's plate");
+        myStack.Push("George's plate");
+        myStack.Push("Pierre's plate");
+        myStack.Push("Kevin's plate");
+        myStack.Push("Solomons plate");
+
+        
+            Console.WriteLine("Amount of plates in the stack : " + myStack.Count);
+            Console.WriteLine("The plate on top is : " + myStack.Peek());
+            Console.WriteLine("The plate on top get's taken off : " + myStack.Pop());
+            Console.WriteLine("The plate on top is : " + myStack.Peek());
+            Console.WriteLine("Amount of plates in the stack : " + myStack.Count);
+            Console.WriteLine("Pierre's plate is still in the stack : " + myStack.Contains("Pierre's plate"));
+            Console.WriteLine();
+            
+            foreach (var StackItem in myStack)
+            {
+                Console.WriteLine(StackItem);
+            }
+
+
+    }
+}
