28fc258409e5145ed93c2b979d99c0c96b6c72b2
kobehoste <kobe.hoste@campuskompas.be>
2025-10-01 14:12:13 +0200

    Add files via upload

---
+++ PutItBackwards.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+public class WordsBackwards
+{
+    static void Main(){
+        
+        Console.WriteLine("Geef een woord in : ");
+        string input = Console.ReadLine();
+        
+        Stack<char> backwards = new Stack<char>();
+
+        foreach (char c in input)
+        {
+            backwards.Push(c);
+        }
+
+        string turnAround = null;
+        while (backwards.Count > 0)
+        {
+            turnAround += backwards.Pop();
+        }
+        Console.WriteLine("Het omgekeerde woord is: "+turnAround);
+
+    }
+}
